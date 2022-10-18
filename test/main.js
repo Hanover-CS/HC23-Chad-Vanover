7ec9e19710278f1e89371370ab8ebc6e567dfd15
vanoverc23 <vanoverc23@hanover.edu>
2022-10-18 14:05:44 -0400

    Got a working chess game on a window ref #7

--- test/main.js
+++ test/main.js
@@ -3,7 +3,11 @@ const { app, BrowserWindow } = require("electron");
 function createWindow () {
     const win = new BrowserWindow({
         width: 1280,
-        height: 600
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
     });
 
     win.loadFile('src/index.html');
--- test/src/script.js
+++ test/src/script.js
@@ -1,1 +1,14 @@
+const { Chess } = require('chess.js')
+
+const chess = new Chess();
+console.log(chess);
+console.log(chess.game_over);
+
+while (!chess.isGameOver()) {
+    const moves = chess.moves();
+    const move = moves[Math.floor(Math.random() * moves.length)]
+    chess.move(move)
+};
+
+console.log(chess.pgn());
 console.log("Hello, world!");
