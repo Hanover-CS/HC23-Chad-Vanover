a69592d594d12b8bb82a694ed6ffa36993a04d71
vanoverc23 <vanoverc23@hanover.edu>
2022-10-25 14:13:37 -0400

    Added chessboard to webpage close #7

--- test/src/script.js
+++ test/src/script.js
@@ -3,7 +3,12 @@ import { Chess } from '../node_modules/chess.js/chess.js';
 import '../node_modules/jquery/dist/jquery.js';
 import '../node_modules/@chrisoakman/chessboardjs/dist/chessboard-1.0.0.js';
 
-var board = new Chessboard();
+const config = {
+    position: 'start',
+    pieceTheme: 'img/{piece}.png'
+  };
+
+const board = new Chessboard('board1', config);
 
 const chess = new Chess();
 console.log(chess);
