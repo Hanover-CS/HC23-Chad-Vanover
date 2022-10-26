e46c4510fc0246450b5445aca5bd6df09fb6e7cb
Chad Vanover <vanoverc23@hanover.edu>
2022-10-26 11:51:43 -0400

    Added random v random

--- test/src/script.js
+++ test/src/script.js
@@ -3,22 +3,38 @@ import { Chess } from '../node_modules/chess.js/chess.js';
 import '../node_modules/jquery/dist/jquery.js';
 import '../node_modules/@chrisoakman/chessboardjs/dist/chessboard-1.0.0.js';
 
-const config = {
-    position: 'start',
-    pieceTheme: 'img/{piece}.png'
-  };
-
-const board = new Chessboard('board1', config);
-
 const chess = new Chess();
 console.log(chess);
 console.log(chess.game_over);
 
-while (!chess.game_over) {
-    const moves = chess.moves();
-    const move = moves[Math.floor(Math.random() * moves.length)];
-    chess.move(move);
+const game = new Chess();
+
+let board = null;
+
+function makeRandomMove () {
+  const possibleMoves = game.moves();
+
+  // exit if the game is over
+  if (game.game_over()) return;
+
+  const randomIdx = Math.floor(Math.random() * possibleMoves.length);
+  game.move(possibleMoves[randomIdx]);
+  board.position(game.fen());
+
+  window.setTimeout(makeRandomMove, 500);
+}
+
+const config = {
+  position: 'start',
+  pieceTheme: 'img/{piece}.png'
 };
+
+board = new Chessboard('board', config);
+console.log(board);
+
+window.setTimeout(makeRandomMove, 500);
+
+
 
 console.log(chess.pgn());
 console.log("Hello, world!");
