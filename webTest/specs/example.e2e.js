4c79bf80ef9ee7aa84aaa3ee281c24b93a114255
Chad Vanover <vanoverc23@hanover.edu>
2022-11-13 17:45:53 -0500

    Write minimal test for square

--- webTest/specs/example.e2e.js
+++ webTest/specs/example.e2e.js
@@ -4,8 +4,13 @@
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
-        const page = browser.url(`http://127.0.0.1:5500/src/index.html`);
-        console.log(page);
+        const page = await browser.url(`http://127.0.0.1:5500/src/index.html`);
+        const $board = await $('#board');
+        // const $square = await $board.$('.square-h2');
+        // const $img = await $('.square-h2 img');
+        expect($board).toBeDisplayed();
+        expect(await $('.square-h2 img').getAttribute('data-piece')).toEqual('wP');
+        await $('.square-h2 img').moveTo();
         // await LoginPage.open();
 
         // await LoginPage.login('tomsmith', 'SuperSecretPassword!');
