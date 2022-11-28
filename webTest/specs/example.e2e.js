29b100ac46d3970c0bc844ba597ccd06058608b9
vanoverc23 <vanoverc23@hanover.edu>
2022-11-28 13:29:18 -0500

    deleted commented out code and added description at top of file

--- webTest/specs/example.e2e.js
+++ webTest/specs/example.e2e.js
@@ -1,24 +1,16 @@
-// const { BrowserView } = require('electron');
-// const LoginPage = require('../pageobjects/login.page');
-// const SecurePage = require('../pageobjects/secure.page');
+// ui tests
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
         const page = await browser.url(`http://127.0.0.1:5500/src/index.html`);
         const $board = await $('#board');
-        // const $square = await $board.$('.square-h2');
-        // const $img = await $('.square-h2 img');
+
         await expect($board).toBeDisplayed();
         await expect(await $('.square-h2 img').getAttribute('data-piece')).toEqual('wP');
         await $('.square-h2 img').moveTo();
         expect(await $$('.highlight')).toHaveLength(3);
         await expect(await $('.square-h2.highlight')).toExist();
-        // await LoginPage.open();
 
-        // await LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        // await expect(SecurePage.flashAlert).toBeExisting();
-        // await expect(SecurePage.flashAlert).toHaveTextContaining(
-        //     'You logged into a secure area!');
     });
 });
 
