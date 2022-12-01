bbc6081aabe28289339bbbbe61485ea81a7ef2bd
vanoverc23 <vanoverc23@hanover.edu>
2022-12-01 14:39:27 -0500

    Testing drag and drop ref #18

--- webTest/specs/example.e2e.js
+++ webTest/specs/example.e2e.js
@@ -27,4 +27,12 @@ describe('Cheesy', () => {
   });
 });
 
-describe('Cheesy', () => {});
+// describe('Cheesy', () => {
+//   it('should move pawn to e4 when user clicks and drags piece', async () => {
+//     await browser.url('http://127.0.0.1:5500/src/index.html');
+//     await $('#board');
+
+//     await $('.square-e2 img').dragAndDrop('.square-e4 img');
+//     expect(await $('.square-e4 img')).toHaveChildren();
+//   });
+// });
