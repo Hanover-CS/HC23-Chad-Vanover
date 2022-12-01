931c7ae0269b9fd53d4903cfe6b183e0e4d06831
vanoverc23 <vanoverc23@hanover.edu>
2022-12-01 14:41:12 -0500

    Lintified highlight

--- src/scripts/highlight.js
+++ src/scripts/highlight.js
@@ -12,14 +12,14 @@
  */
 export function highlightMoves (square, moves) {
   // exit if there are no moves available for this square
-  if (moves.length === 0) return
+  if (moves.length === 0) return;
 
   // highlight the square they moused over
-  greySquare(square)
+  greySquare(square);
 
   // highlight the possible squares for this piece
   for (let i = 0; i < moves.length; i++) {
-    greySquare(moves[i].to)
+    greySquare(moves[i].to);
   }
 }
 
@@ -28,10 +28,10 @@ export function highlightMoves (square, moves) {
  * @param {String} square - An html string for a square on a chessboard
  */
 function greySquare (square) {
-  const $square = $('#board .square-' + square)
-  $square.addClass('highlight')
+  const $square = $('#board .square-' + square);
+  $square.addClass('highlight');
 }
 
 export function removeGreySquares () {
-  $('#board .square-55d63').removeClass('highlight')
+  $('#board .square-55d63').removeClass('highlight');
 }
