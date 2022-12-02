e12002fd1dc58f4275b8f45127b5765041a56c1b
vanoverc23 <vanoverc23@hanover.edu>
2022-12-02 12:29:43 -0500

    Added ui tests ref #18

--- webTest/specs/example.e2e.js
+++ webTest/specs/example.e2e.js
@@ -27,12 +27,36 @@ describe('Cheesy', () => {
   });
 });
 
-// describe('Cheesy', () => {
-//   it('should move pawn to e4 when user clicks and drags piece', async () => {
-//     await browser.url('http://127.0.0.1:5500/src/index.html');
-//     await $('#board');
+describe('Cheesy', () => {
+  it('should not highlight when mouse hovers over invalid piece', async () => {
+    await browser.url('http://127.0.0.1:5500/src/index.html');
+    await $('#board');
 
-//     await $('.square-e2 img').dragAndDrop('.square-e4 img');
-//     expect(await $('.square-e4 img')).toHaveChildren();
-//   });
-// });
+    await $('.square-a7 img').moveTo();
+    await !expect(await $('.square-h2.highlight')).toExist();
+  });
+});
+
+describe('Cheesy', () => {
+  it('should move pawn to e4 when user clicks and drags piece', async () => {
+    await browser.url('http://127.0.0.1:5500/src/index.html');
+    await $('#board');
+
+    (await $('.square-e2 img')).dragAndDrop($('.square-e4'));
+    expect(await $('.square-e4 img')).toExist();
+  });
+});
+
+describe('Cheesy', () => {
+  it('should discard black pawn when white pawn captures', async () => {
+    await browser.url('http://127.0.0.1:5500/src/index.html');
+    await $('#board');
+
+    (await $('.square-e2 img')).dragAndDrop($('.square-e4'));
+    expect(await $('.square-e4 img')).toExist();
+    (await $('.square-d7 img')).dragAndDrop($('.square-d5'));
+    expect(await $('.square-d5 img')).toExist();
+    (await $('.square-e4 img')).dragAndDrop($('.square-d5 img'));
+    expect((await $('.square-d5 img')).getAttribute('data-piece')).toExist('wP');
+  });
+});
