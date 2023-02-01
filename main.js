9d255cbac854da59aa42a0b035c5bc6166355024
Chad Vanover <vanoverc23@hanover.edu>
2023-02-01 15:43:31 -0500

    Added support for multiple windows ref #11

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const { app, BrowserWindow } = require("electron");
 
-function createWindow () {
-    const win = new BrowserWindow({
+const windows = new Set();
+
+function createWindow() {
+    let win = new BrowserWindow({
         width: 1280,
         height: 600,
         webPreferences: {
@@ -10,10 +12,21 @@ function createWindow () {
         }
     });
 
+    windows.add(win)
+
+    win.on("closed", () => {
+        windows.delete(win);
+        win = null;
+    });
+
     win.loadFile('src/index.html');
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+    if (windows.size == 0) {
+        createWindow()
+    }
+});
 
 app.on("window-all-closed", () => {
     if (process.platform !== "darwin") app.quit();
